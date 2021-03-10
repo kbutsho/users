fbe199f246cd7ae24677b5099dfad8796bbf2e40
Kbutsho <kbutsho@yahoo.com>
2021-03-10 22:49:47 +0600

    update2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import NoMatch from './components/NoMatch/NoMatch';
 import FriendDetails from './components/FriendDetail/FriendDetails';
